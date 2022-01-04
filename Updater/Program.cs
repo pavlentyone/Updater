aa0a5ab2ef399ea6210003f16a20c096bc2c3f15
pavlentyone <96060581+pavlentyone@users.noreply.github.com>
2022-01-04 14:32:27 +0300

    init: load files

---
+++ Updater/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace Updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            args = new string[3]
+            {
+                 @"\\10.3.1.164\D$\TrainsManagement-hatelinq2\TrainsManagement\bin\Debug\net5.0",
+                 @"D:\\TrainsManagement\TrainsManagement\bin\Debug\net5.0-windows",
+                 "TrainsManagement.exe"
+            };
+
+            if (args.Length < 3)
+            {
+                Console.WriteLine($"Amounts of arguments should 3 or more");
+                Console.Read();
+                return;
+            }
+
+            Updater.updatePath = args[0];
+            Updater.currentPath = args[1];
+            Updater.exeFile = args[2];
+
+            var fullExeName = $"{Updater.currentPath}\\{Updater.exeFile}";
+
+            var check1 = Directory.Exists(Updater.updatePath);
+            var check2 = Directory.Exists(Updater.currentPath);
+            var check3 = args.Length >= 3 ? 
+                File.Exists(fullExeName) || File.Exists($"{Updater.updatePath}\\{Updater.exeFile}") : 
+                false;
+
+            Console.WriteLine($"Folder from\n{check1}\nFolder to\n{check2}\nFile exe\n{check3}");
+
+            if (!check1 || !check2 || !check3)
+            {
+                Console.Read();
+                return;
+            }
+
+            var isInUse = Updater.IsFileInUse(fullExeName);
+
+            if (isInUse)
+            {
+                Console.WriteLine($"Folder from\n{check1}\nFolder to\n{check2}\nFile exe\n{check3}");
+                Console.Read();
+                return;
+            }
+
+            var sw = new Stopwatch();
+
+            sw.Start();
+            Updater.ReplaceFiles();
+            sw.Stop();
+            Console.WriteLine($"Sync: {sw.ElapsedMilliseconds}");
+
+            //sw.Start();
+            //Updater.ReplaceFilesAsync().GetAwaiter().GetResult();
+            //sw.Stop();
+            //Console.WriteLine($"Async: {sw.ElapsedMilliseconds}");
+
+            if(check3)
+                Updater.OpenFile(fullExeName);
+        }
+    }
+}
---
+++ Updater/Updater.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace Updater
+{
+    public static class Updater
+    {
+        public static string updatePath = "C:\\Users\\USeR-050716\\Desktop\\net5.0-windows";
+        public static string currentPath = Directory.GetCurrentDirectory();
+        public static string exeFile = "TrainsManagement.exe";
+        public async static Task ReplaceFilesAsync()
+        {
+
+            var updateTask = Task.Run(() =>
+                (from f in Directory.GetFiles(updatePath)
+                 select new FileInfo(f))
+                .ToArray());
+
+            var currentTask = Task.Run(() =>
+                (from f in Directory.GetFiles(currentPath)
+                 select new FileInfo(f))
+                .ToArray());
+
+            var updateFiles = await updateTask;
+            var currentFiles = await currentTask;
+
+            Parallel.ForEach(updateFiles, 
+                (file) => file.CopyTo(currentPath + "\\" + file.Name, true));
+        }
+
+        public static void ReplaceFiles()
+        {
+            var updateFiles = 
+                (from f in Directory.GetFiles(updatePath)
+                 select new FileInfo(f))
+                .ToArray();
+
+            var currentFiles = 
+                (from f in Directory.GetFiles(currentPath)
+                 select new FileInfo(f))
+                .ToArray();
+
+            foreach(var file in updateFiles)
+                file.CopyTo(currentPath + "\\" + file.Name, true);
+        }
+
+        // Just open file safely. Returns excaption if file is busy
+        public static Exception OpenFile(string filename)
+        {
+            try
+            {
+                new Process
+                {
+                    StartInfo = new ProcessStartInfo(filename)
+                    {
+                        UseShellExecute = true
+                    }
+                }.Start();
+            }
+            catch (Exception exc)
+            {
+                return exc;
+            }
+
+            return null;
+        }
+
+        // Checks if file is in use
+        public static bool IsFileInUse(string filename)
+        {
+            try
+            {
+                File.Open(
+                  filename, FileMode.OpenOrCreate,
+                  FileAccess.Read, FileShare.None)
+                  .Close();
+            }
+            catch
+            { return true; }
+
+            return false;
+        }
+    }
+}
